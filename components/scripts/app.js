112025eca8ff7bac0217d056c072c45e0693a5a5
Amar <amar.shahukhal@gmail.com>
2020-01-24 11:48:17 +1100

    Initial setup of Crestron project

---
+++ components/scripts/app.js
@@ -0,0 +1,9 @@
+import AOS from 'aos';
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('test');
+    console.log('test test');
+    AOS.init();
+});
---
+++ gulpfile.babel.js/index.js
@@ -0,0 +1,8 @@
+import { watch } from './tasks/watch';
+import { build } from './tasks/build';
+import { copy } from './tasks/copy';
+import { clean } from './tasks/clean';
+exports.watch = watch;
+exports.build = build;
+exports.copy = copy;
+exports.clean = clean;
