440c7a4f976b2d21eac7bd91c75c4bad1531e92d
Shivakumar <shimar007@gmail.com>
2020-01-30 17:03:00 +1100

    css changes

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $LPname = "Crestron - Report";
 
-$conn = new mysqli('localhost','root', 'Bang2020!', 'crestron_ten_four'); 
+$conn = new mysqli('localhost','crestron-p', 'WjLq5rXGRBWLXPVw', 'crestron-p'); 
 if ($conn->connect_error) {
     trigger_error('Database connection failed: ' . $conn->connect_error, E_USER_ERROR);
 }
