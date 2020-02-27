07ae3698773d2fb31a82067da04bc5191bb9bb5b
francisbang <59549139+francisbang@users.noreply.github.com>
2020-02-27 14:03:11 +1100

    changes to footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <footer id="footer" class="footer">
         <div class="footer-container">
             <div class="container">
-                <div class="footer-contents">
+                <!-- <div class="footer-contents">
                     <div class="left-footer">
                         <a href="https://www.crestron.com/" target="_blank"><img src="images/logo-crestron.png" width="177" alt="Crestron Partners"></a>
                         <a href="https://tenfour.nz/" target="_blank"><img src="images/logo-ten-four.svg" width="111" alt="Ten Four"></a>
@@ -10,6 +10,15 @@
                     <div class="right-footer">
                         <span class="copyright">&copy;  2020 Crestron Electronics, Inc.</span>
                     </div>
+                </div> -->
+                <p>&copy;2020 Crestron Electronics, Inc.</p>
+                <p>All brand names, product names and trademarks are the property of their respective owners. Certain trademarks, registered trademarks, and trade names may be used to refer to either the entities claiming the marks and names or their products. Crestron disclaims any proprietary interest in the marks and names of others. Crestron is not responsible for errors in typography or photography. Specifications are subject to change without notice.</p>
+                <div class="footer-legal-links">
+                <a href="https://www.crestron.com/en-us/legal/patents">Patents </a>
+                <a href="https://www.crestron.com/en-us/legal">Legal</a>
+                <a href="https://www.crestron.com/crestroneuropeterms">Crestron Europe Terms</a>
+                <a href="https://www.crestron.com/en-us/legal/privacy-policy">Privacy Policy</a>
+                <a href="https://www.crestron.com/en-us/legal/website-terms-of-use">Terms of Use</a>
                 </div>
             </div>
         </div>
