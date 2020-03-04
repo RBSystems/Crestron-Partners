3f0ad156cc9e5f0ed4ba87826505c6b82c9cde0e
francisbang <59549139+francisbang@users.noreply.github.com>
2020-03-05 10:52:32 +1100

    footer

--- footer.php
+++ footer.php
@@ -14,11 +14,11 @@
                 <p>&copy;2020 Crestron Electronics, Inc.</p>
                 <p>All brand names, product names and trademarks are the property of their respective owners. Certain trademarks, registered trademarks, and trade names may be used to refer to either the entities claiming the marks and names or their products. Crestron disclaims any proprietary interest in the marks and names of others. Crestron is not responsible for errors in typography or photography. Specifications are subject to change without notice.</p>
                 <div class="footer-legal-links">
-                <a href="https://www.crestron.com/en-us/legal/patents">Patents </a>
-                <a href="https://www.crestron.com/en-us/legal">Legal</a>
-                <a href="https://www.crestron.com/crestroneuropeterms">Crestron Europe Terms</a>
-                <a href="https://www.crestron.com/en-us/legal/privacy-policy">Privacy Policy</a>
-                <a href="https://www.crestron.com/en-us/legal/website-terms-of-use">Terms of Use</a>
+                <a target="_blank" href="https://www.crestron.com/en-us/legal/patents">Patents </a>
+                <a target="_blank" href="https://www.crestron.com/en-us/legal">Legal</a>
+                <a target="_blank" href="https://www.crestron.com/crestroneuropeterms">Crestron Europe Terms</a>
+                <a target="_blank" href="https://www.crestron.com/en-us/legal/privacy-policy">Privacy Policy</a>
+                <a target="_blank" href="https://www.crestron.com/en-us/legal/website-terms-of-use">Terms of Use</a>
                 </div>
             </div>
         </div>
